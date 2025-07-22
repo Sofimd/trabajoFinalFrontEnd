ce66ccef91185ea9f373ab49643124b146da51fb
So Do <sofi.doffi.sd@gmail.com>
2025-07-21 22:34:54 -0300

    js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*
+for (let i = 0; i < productos.length; i++) {
+    const producto = productos[i];
+
+    divProductos.insertAdjacentHTML("afterbegin",
+
+        `
+        <div class="producto">
+            <img src=${producto.imagen}" alt="${producto.nombre}">
+            <div class="producto-contenido">
+                <h4>${producto.nombre}</h4>
+                <span>Código: ${producto.id}</span>
+                <span>Precio: $ ${producto.precio}</span>
+                <button class="btn-comprar" type="button" data-id="${producto.id}">Comprar</button>
+            </div>
+        </div>
+        `    
+    );
+}
+
+*/
