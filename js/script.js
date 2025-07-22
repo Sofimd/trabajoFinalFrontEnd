cd71da15eeb62ab1279411ca5483492ccfb8a99d
So Do <sofi.doffi.sd@gmail.com>
2025-07-22 01:33:17 -0300

    js lista, for

--- js/script.js
+++ js/script.js
@@ -1,7 +1,87 @@
-/*
-for (let i = 0; i < productos.length; i++) {
+console.log("Hola")
+let productos = [
+    {
+        nombre: "remera blanca 1",
+        tieneDescuento: true;
+        categoria: "lisa",
+        sexo: "hombre"
+    },
+
+    {
+        nombre: "remera gris 1",
+        tieneDescuento: false,
+        categoria: "lisa",
+        sexo: "hombre"
+    },
+
+    {
+        nombre: "remera negra 1",
+        tieneDescuento: true,
+        categoria: "lisa",
+        sexo: "hombre"
+    },
+
+    {
+        nombre: "remera verde 1",
+        tieneDescuento: false,
+        categoria: "lisa",
+        sexo: "hombre"
+    },
+
+    {
+
+        nombre: "remera blanca 2",
+        tieneDescuento: true,
+        categoria: "lisa",
+        sexo: "mujer"
+    },
+
+    {
+        nombre: "remera gris 2",
+        tieneDescuento: false,
+        categoria: "lisa",
+        sexo: "mujer"
+    },
+
+    {
+        nombre: "remera negra 2",
+        tieneDescuento: true,
+        categoria: "lisa",
+        sexo: "mujer"
+    },
+
+    {
+        nombre: "remera verde 2",
+        tieneDescuento: false,
+        categoria: "lisa",
+        sexo: "mujer"
+    },
+
+    {
+        nombre: "remera estampa 1",
+        tieneDescuento: true,
+        categoria: "estampada",
+        sexo: "hombre"
+    },
+
+    {
+        nombre: "remera estampa 2",
+        tieneDescuento: false,
+        categoria: "estampada",
+        sexo: "mujer"
+    }
+] 
+
+let todosLosProductos = document.getElementById("todosLosProductos");
+for (let contador = 0; contador < productos.lenght;contador++) {
+    todosLosProductos.innerHTML = todosLosProductos.innerHTML + "Producto" + (contador + 1) 
+    + "- nombre: " + productos[contador].nombre + "- descuento: " + productos[contador].tieneDescuento + "<br>"; 
+} 
+
+/* for (let i = 0; i < productos.length; i++) {
     const producto = productos[i];
 
+    
     divProductos.insertAdjacentHTML("afterbegin",
 
         `
@@ -19,6 +99,3 @@ for (let i = 0; i < productos.length; i++) {
 }
 
 */
-
-console.log("Ok")
-
